cc223287f42391d9478396db38a55136e279de4f
Josephat <jogwayi@users.noreply.github.com>
2025-08-29 12:56:41 +0300

    Update resource.ts
    bug fixes

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,7 +2,7 @@ import { defineStorage } from '@aws-amplify/backend';
 
 const branch = process.env.AWS_BRANCH || 'dev';
 
-const storage = defineStorage({
+export const storage = defineStorage({
   name: branch === 'main' ? 'prodStorage' : 'devStageStorage',
   access: (allow) => ({
     'public/*': [
