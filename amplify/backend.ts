46d1e9992bcaaa7e7c13b75c0ff5823cc0606064
Josephat <jogwayi@users.noreply.github.com>
2025-08-29 16:10:55 +0300

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -13,4 +13,10 @@ const backend = defineBackend({
 });
 
 const exactBucketName = branch === 'main' ? 'existing-prod-bucket-name': 'existing-dev-bucket-name'
-backend.storage.resources.bucket.addPropertyOverride('BucketName', exactBucketName);
+
+console.log("Bucket", JSON.stringify(backend.storage.resources.bucket))
+
+backend.storage.resources.bucket = new s3.Bucket(backend.storage.resources.bucket, 'MyCustomBucket', {
+      bucketName: exactBucketName      
+    });
+// backend.storage.resources.bucket.addPropertyOverride('BucketName', exactBucketName);
