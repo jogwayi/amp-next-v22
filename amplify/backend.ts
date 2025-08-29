345600a91c48c15355b8d934b420920549db1d47
Josephat <jogwayi@users.noreply.github.com>
2025-08-29 11:57:37 +0300

    Update backend.ts
    Custom storage definition

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,14 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { devStageStorage } from './storage/dev-stage-resource';
+import { prodStorage } from './storage/prod-resource';
+
+const branch = process.env.AWS_BRANCH || 'dev';
+
 
 defineBackend({
   auth,
   data,
+  storage: branch === 'main' ? prodStorage : devStageStorage
 });
