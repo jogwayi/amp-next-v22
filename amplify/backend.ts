f009106ea8c09c14ef443a9dea890d26cfbceb1b
Josephat <jogwayi@users.noreply.github.com>
2025-08-29 12:09:33 +0300

    Update backend.ts
    Simpleify Amplify storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,14 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { devStageStorage } from './storage/dev-stage-resource';
-import { prodStorage } from './storage/prod-resource';
+import { storage } from './storage/resource';
 
 const branch = process.env.AWS_BRANCH || 'dev';
-
 
 defineBackend({
   auth,
   data,
-  storage: branch === 'main' ? prodStorage : devStageStorage
+  storage
 });
