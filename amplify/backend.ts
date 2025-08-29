79a7b63ddcf3da37a8741caf4277692baa3afa5b
Josephat <jogwayi@users.noreply.github.com>
2025-08-29 16:11:18 +0300

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
