53ef0d180e0329ec9b6524a7a71e8cdb187243e8
Josephat <jogwayi@users.noreply.github.com>
2025-08-29 15:22:44 +0300

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,8 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
+import { Stack } from 'aws-cdk-lib';
+
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 
 const branch = process.env.AWS_BRANCH || 'dev';
 
-export const storage = defineStorage({
+const storage = defineStorage({
   name: branch === 'main' ? 'prodStorage' : 'devStageStorage',
   access: (allow) => ({
     'public/*': [
@@ -17,3 +20,17 @@ export const storage = defineStorage({
     ]
   })
 });
+const s3Bucket = storage.resources.bucket;
+
+const cfnBucket = s3Bucket.node.defaultChild as CfnBucket;
+
+const storageStack = Stack.of(cfnBucket);
+const existingBucket = Bucket.fromBucketName(
+  storageStack,
+  'ExistingBucket',
+  branch === 'main' ? 'existing-prod-bucket-name': 'existing-dev-bucket-name'
+);
+
+storage.resources.bucket = existingBucket;
+
+export default storage
